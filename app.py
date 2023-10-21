78ba5b8efca092124c69c74a7f1850ef16f9e66c
dev.kthines <dev.kthines@gmail.com>
2023-10-21 15:41:16 -0700

    commit 2. staging to fine-tune the model and then add a front-end before deployment with aws

--- app.py
+++ app.py
@@ -1,32 +1,45 @@
-﻿from flask import Flask, render_template, request
+﻿from flask import Flask, render_template, request, jsonify
+import os
 import openai
 import asyncio
+import threading
 
 app = Flask(__name__)
+openai_api_key = os.getenv("OPENAI_API_KEY")
 
-# Set your OpenAI API key here
-openai.api_key = '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'
+# Check if the API key is set
+if openai_api_key is None:
+    print("Error: OPENAI_API_KEY environment variable not set.")
+else:
+    print(f"API Key: {openai_api_key}")
 
-async def ask_openai_gpt(question):
-    response = await asyncio.to_thread(
-        openai.Completion.create,
-        engine="davinci-002",
-        prompt=question,
-        max_tokens=150,
-        n=1,
-        stop=None
+# Now you can use openai_api_key in your OpenAI API calls
+def get_openai_response(messages):
+    response =  openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+        temperature=0.7,
+        max_tokens=150
     )
-    return response.choices[0].text.strip()
+    return response.choices[0].message['content']
+
+def run_asyncio_loop(loop, messages):
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(get_openai_response(messages))
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/ask', methods=['POST'])
-async def ask():
-    user_input = request.form['user_input']
-    response_text = await ask_openai_gpt(user_input)
-    return response_text
+@app.route('/get_response', methods=['POST'])
+def get_response():
+    user_message = request.form['user_message']
+    messages = [
+        {"role": "system", "content": "You joke around a lot."},
+        {"role": "user", "content": user_message}
+    ]
+    response =  get_openai_response(messages)
+    return jsonify({"response": response})
 
 if __name__ == '__main__':
     app.run(debug=True)
